67949d0f042e5552dd954951ade327a3c06dfa12
ShivangNagta <shivangnag@gmail.com>
2023-12-23 20:11:46 +0530

    3rd modified

--- 3_(LongestSubstring).py
+++ 3_(LongestSubstring).py
@@ -1,24 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        checkDic = {}
-        currentHighest = 0
-        allHigh=[0] 
-        for index,i in enumerate(s):
-            if i in checkDic.keys():
-                allHigh.append(currentHighest)
-                currentHighest = index - checkDic[i]
-                dicToListValues=[]
-                for t in checkDic:
-                    if checkDic[t]<checkDic[i]:
-                        dicToListValues.append(t)
-                for t in dicToListValues:
-                    del checkDic[t]
-                checkDic[i] = index
-            else:
-                currentHighest += 1
-                checkDic[i]=index
-        allHigh.append(currentHighest)        
-        return max(allHigh)
-     
-obj=Solution()
-print(obj.lengthOfLongestSubstring("abaab"))
+        char_index = {}
+        start = 0
+        max_length = 0
+
+        for end, char in enumerate(s):
+            if char in char_index and char_index[char] >= start:
+                start = char_index[char] + 1
+
+            char_index[char] = end
+            max_length = max(max_length, end - start + 1)
+
+        return max_length
